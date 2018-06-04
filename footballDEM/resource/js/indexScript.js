ed58bd25455b417e487767af4ca6cf8970719ca6
Unknown <wa.morenoramirez@ugto.mx>
2018-06-04 11:23:11 -0500

    Cargar datos desde el inicio de la página
    Se agregó un archivo .php para hacer una consulta y, mediante Javascript, cargar los datos de la última jornada al abrir la página.

--- footballDEM/resource/js/indexScript.js
+++ footballDEM/resource/js/indexScript.js
@@ -4,7 +4,7 @@ function hello(data1, data2){
 }
 
 function clearBoxes(){
-    loadData();
+//    loadData();
     var usr = document.getElementById("txtUser").value = "";
     var pwd = document.getElementById("txtPassword").value = "";
 }
